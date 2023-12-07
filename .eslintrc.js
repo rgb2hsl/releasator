c6de001c4960ae5086dfaebf7e136e25028ff2b9
rgb2hsl <ru.web.designer@gmail.com>
2023-12-07 20:36:57 +0400

    added editorconfig, eslint and prettier added some packages added idea things changed wrangler.toml

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "serviceworker": true,
-        "node": true
-    },
-    "extends": "standard-with-typescript",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    serviceworker: true,
+    node: true
+  },
+  extends: [
+    'standard-with-typescript', 'prettier'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    "@typescript-eslint/no-misused-promises": "off"
+  }
 }
