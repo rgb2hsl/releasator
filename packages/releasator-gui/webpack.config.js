cfb10bd6cc84911925f3310e116d162b5a3986f2
rgb2hsl <ru.web.designer@gmail.com>
2024-01-07 21:08:43 +0400

    proper webpack env handling

--- packages/releasator-gui/webpack.config.js
+++ packages/releasator-gui/webpack.config.js
@@ -2,42 +2,43 @@ const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
-const env = process.env.NODE_ENV;
 
-module.exports = {
-    mode: "development",
-    devtool: "inline-source-map",
-    entry: "./src/index.tsx",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "index.js"
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-        extensionAlias: {
-            ".js": [".js", ".ts"],
-            ".cjs": [".cjs", ".cts"],
-            ".mjs": [".mjs", ".mts"]
-        }
-    },
-    module: {
-        rules: [
-            {test: /\.([cm]?ts|tsx)$/, loader: "ts-loader"}
-        ]
-    },
-    devServer: {
-        historyApiFallback: true
-    },
-    plugins: [
-        new NodePolyfillPlugin(),
-        new CopyPlugin({
-            patterns: [
-                {from: "./static"},
-                {from: "../../node_modules/react95/dist/fonts", to: "./fonts"}
+module.exports = (env) => {
+    return {
+        mode: "development",
+        devtool: "inline-source-map",
+        entry: "./src/index.tsx",
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "index.js"
+        },
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"],
+            extensionAlias: {
+                ".js": [".js", ".ts"],
+                ".cjs": [".cjs", ".cts"],
+                ".mjs": [".mjs", ".mts"]
+            }
+        },
+        module: {
+            rules: [
+                {test: /\.([cm]?ts|tsx)$/, loader: "ts-loader"}
             ]
-        }),
-        new Dotenv({
-            path: `./.env${env === "production" ? "" : ".development"}`
-        })
-    ]
+        },
+        devServer: {
+            historyApiFallback: true
+        },
+        plugins: [
+            new NodePolyfillPlugin(),
+            new CopyPlugin({
+                patterns: [
+                    {from: "./static"},
+                    {from: "../../node_modules/react95/dist/fonts", to: "./fonts"}
+                ]
+            }),
+            new Dotenv({
+                path: `./.env${env.production ? "" : ".development"}`
+            })
+        ]
+    }
 };
